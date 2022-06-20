235f1417791d36cc7ef3ac83204f763389e56f1f
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-06-20 21:40:15 +0300

    initial commit

---
+++ src/main/java/com/example/ithillelspringboot/IthillelSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ithillelspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthillelSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthillelSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ithillelspringboot/IthillelSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ithillelspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthillelSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
